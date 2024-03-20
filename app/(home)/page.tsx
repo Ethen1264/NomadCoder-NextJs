fc9ab1cc9a0c4aecfa86b836d80ae71522ef227f
Ethen1264 <jxorhks86909763@gmail.com>
2024-03-20 15:15:10 +0900

    add #3.2 Server Side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,18 +1,19 @@
-'use client';
+import { resolve } from 'path';
 
-import { useEffect, useState } from 'react';
+export const metadata = {
+  title: 'Home',
+};
 
-export default function Tomato() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch('https://nomad-movies.nomadcoders.workers.dev/movies');
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return <div>{isLoading ? 'Loading...' : JSON.stringify(movies)}</div>;
+const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  console.log('im fetching');
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
