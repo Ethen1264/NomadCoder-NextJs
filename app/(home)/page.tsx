d68441a1f5a298721ba933eec2efb198ea41243c
정태관 <ethen1264@jeongtaegwan-ui-MacBookPro.local>
2024-03-20 11:29:41 +0900

    add #3.1 Client Side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,18 @@
+'use client';
 
-export const metadata = {
-  title: 'Home',
-}
+import { useEffect, useState } from 'react';
 
 export default function Tomato() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch('https://nomad-movies.nomadcoders.workers.dev/movies');
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return <div>{isLoading ? 'Loading...' : JSON.stringify(movies)}</div>;
 }
